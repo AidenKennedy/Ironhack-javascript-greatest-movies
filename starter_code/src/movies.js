7ed7160d926daf97b48fc2cd425a9099e300c512
Kennedy Aiden <20185@columbushs.com>
2019-11-21 09:08:15 -0500

    commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,9 +2,47 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(array) {
+
+    let copy = [...array];
+
+    copy.sort((a, b) => {
+        if (a.year < b.year) {
+            return -1;
+        }
+        else if (b.year < a.year) {
+            return 1;
+        }
+        else {
+            if (a.title < b.title) {
+                return -1;
+            }
+            else {
+                return 0;
+            }
+        }
+    })
+    return copy;
+};
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
+function howManyMovies(array) {
+
+    let stevenMovies = array.filter((eachMovie) => {
+        return eachMovie.director == 'Steven Spielberg' && eachMovie.genre.includes('Drama');
+    })
+
+    return stevenMovies.length;
+}
+
+console.log(howManyMovies(movies));
+
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(array) {
+    let copy = 
+}
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
